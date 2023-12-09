affadbe978212a8ed159e6fdabed73bd94d823e5
Yuri Santos <yuri.rps@gmail.com>
2023-12-09 19:02:42 -0300

    estrutura inicial

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+/*
+
+WordPress REST API Dogs
+https://www.origamid.com/curso/wordpress-rest-api-dogs
+
+Endpoint para Teste
+https://dogsapi.origamid.dev/json/
+
+Dados serão apagados a cada 10 minutos
+
+Usuário padrão
+
+username: dog
+password: dog
+
+*/ 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
